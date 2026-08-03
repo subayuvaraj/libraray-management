bf934d4ca875d2e3f523e35218650bc4c9874bf1
subayuvaraj <subacourses@gmail.com>
2026-08-02 22:40:58 -0500

    Initial Commit

---
+++ src/main/java/com/work/LibraryManagementApplication.java
@@ -0,0 +1,12 @@
+package com.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.metrics.ApplicationStartup;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementApplication.class, args);
+    }
+}
